f21d61efdc6f3974eb94070dd1073fdaf5f00a49
Potrek Siekierski <ec2-user@ip-172-31-23-219.eu-central-1.compute.internal>
2018-04-19 17:39:16 +0000

    kod z zajec

--- web/app.py
+++ web/app.py
@@ -1,10 +1,16 @@
 
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "here will be my upload interface"
-
+@app.route("/prepare")
+def prepere():
+  return render_template(
+    'prepare.html',
+    invitation="the only limit is yourseff"
+)
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", port=5000, debug=True)
+  app.run(host="0.0.0.0", port=8080, debug=True)
